93d64f8669c246ad906c9457583f8d9d02c66a24
IvanAbacaCS <ivan.abaca@wbdcontractor.com>
2024-08-18 21:08:22 -0300

    Street Numbers and matrix (not finished)

---
+++ JavaClasses/src/Class1/MatrixRotate.java
@@ -0,0 +1,25 @@
+package Class1;
+
+public class MatrixRotate {
+	
+	/**
+	 * Sea una matriz de enteros de tamaño n x n. Ésta representa una imagen, y se pide rotarla 90 grados en sentido horario.
+	 * Se debe rotar la imagen en su lugar, lo que significa que se debe modificar directamente la matriz de entrada.
+	 * NO asignar otra matriz 2D y realizar la rotación.
+	 * 	Entrada:	Salida:
+		1 2 3		7 4 1
+		4 5 6		8 5 2
+		7 8 9		9 6 3
+	 * 
+	 * Restricciones
+	 * n == imagen.length == imagen[i].length
+	 * 1 <= n <= 20
+	 * -1000 <= imagen[i][j] <= 1000
+	 */
+	
+	public static void rotar90(int[][] imagen) { 
+		// This function rotates a matrix
+		// (...)
+	}
+	
+}
---
+++ JavaClasses/src/Class1/StreetNumbers.java
@@ -0,0 +1,30 @@
+package Class1;
+public class StreetNumbers {
+	
+	/**
+	 * Una programadora de computadoras vive en una calle con casas numeradas consecutivamente (desde 1) 
+	 * por un lado de la calle. Cada noche ella sale a pasear a su perro dejando su casa, yendo al azar a 
+	 * la izquierda o a la derecha, camina hasta el final de la calle y vuelve.
+	 * Una noche suma los números de las casas que pasan (excluyendo la suya). 
+	 * La siguiente vez que camina, comienza por el otro lado repitiendo la suma y encuentra, 
+	 * para su asombro, que las dos sumas son iguales. Aunque esto se determina en parte por 
+	 * su número de casa y en parte por el número de casas en la calle, ella sin embargo siente 
+	 * que esta es una propiedad deseable para su casa y decide que todas sus casas subsecuentes 
+	 * tendrán esa propiedad.
+	 * Escribe un programa para encontrar los pares de números que satisfagan esta condición. 
+	 * Para comenzar su lista los dos primeros pares son: (número de casa, último número):
+	 * Desarrollar una clase con tres (o más) métodos, que resuelvan el mismo problema pero 
+	 * utilizando aproximaciones similares. 
+	 * Al menos uno de ellos debe utilizar un algoritmo 
+	 * cuadrático, uno lineal y otro constante
+	 * @param lastHouse
+	 * @return
+	 */
+	public static int findIdealHouse(int lastHouse) {
+		// Returns the ideal house
+		// (...)
+		
+		return 0;
+	}
+	
+}
