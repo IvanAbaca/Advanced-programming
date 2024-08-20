bb7f6819abe4ca3c78adb6ef3c3abcec23e63614
IvanAbacaCS <ivan.abaca@wbdcontractor.com>
2024-08-20 17:31:25 -0300

    Matrix rotate finished

--- JavaClasses/src/Class1/MatrixRotate.java
+++ JavaClasses/src/Class1/MatrixRotate.java
@@ -1,25 +1,45 @@
 package Class1;
 
 public class MatrixRotate {
-	
-	/**
-	 * Sea una matriz de enteros de tamaño n x n. Ésta representa una imagen, y se pide rotarla 90 grados en sentido horario.
-	 * Se debe rotar la imagen en su lugar, lo que significa que se debe modificar directamente la matriz de entrada.
-	 * NO asignar otra matriz 2D y realizar la rotación.
-	 * 	Entrada:	Salida:
-		1 2 3		7 4 1
-		4 5 6		8 5 2
-		7 8 9		9 6 3
-	 * 
-	 * Restricciones
-	 * n == imagen.length == imagen[i].length
-	 * 1 <= n <= 20
-	 * -1000 <= imagen[i][j] <= 1000
-	 */
-	
-	public static void rotar90(int[][] imagen) { 
-		// This function rotates a matrix
-		// (...)
+
+	public static void rotar90(int[][] imagen) {
+        rotarRecursivo(imagen, 0, imagen.length);
+    }
+
+    private static void rotarRecursivo(int[][] imagen, int inicio, int tamaño) {
+        if (tamaño <= 1)
+            return;
+
+        int fin = inicio + tamaño - 1;
+
+        for (int i = 0; i < tamaño - 1; i++) {
+            int temp = imagen[inicio][inicio + i];
+            imagen[inicio][inicio + i] = imagen[fin - i][inicio];
+            imagen[fin - i][inicio] = imagen[fin][fin - i];
+            imagen[fin][fin - i] = imagen[inicio + i][fin];
+            imagen[inicio + i][fin] = temp;
+        }
+        rotarRecursivo(imagen, inicio + 1, tamaño - 2);
+    }
+    
+    public static void main(String[] args) {
+    	int[][] m1 = {{1,2,3},{4,5,6},{7,8,9}};
+    	int[][] m2 = {{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
+    	
+    	rotar90(m1);
+    	mostrarMatriz(m1);
+    	
+    	rotar90(m2);
+    	mostrarMatriz(m2);
 	}
-	
+    
+    public static void mostrarMatriz(int[][]m) {
+    	System.out.println();
+    	for(int[] xl : m)
+		{
+			for(int x : xl)
+				System.out.print(x + "\t");
+			System.out.println();
+		}
+    }
 }
