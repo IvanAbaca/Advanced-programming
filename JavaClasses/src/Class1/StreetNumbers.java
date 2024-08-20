8f6161818041af1bb167bdb42966ef93b14bb129
IvanAbacaCS <ivan.abaca@wbdcontractor.com>
2024-08-20 18:49:20 -0300

    StreetNumbers finished (i think)

--- JavaClasses/src/Class1/StreetNumbers.java
+++ JavaClasses/src/Class1/StreetNumbers.java
@@ -1,19 +1,64 @@
 package Class1;
 
-
 public class StreetNumbers {
+	
+	public static void main(String[] args) {
+		
+		StreetNumbers sn = new StreetNumbers();
+		int test = 49;
+		
+		System.out.println(sn.cuadratica(test));
+		System.out.println(sn.lineal(test));
+		System.out.println(sn.constante(test));
+		
+		
+	}
+	
 	int cuadratica(int n) {
-		// (...)
-		return 0;
+		int izq;
+		int der;
+		
+		for(int i=1; i<n; i++) {
+			izq = 0;
+			der = 0;
+			
+			for(int j=0;j<i;j++)
+				izq+=j;
+			for(int k=i+1;k<=n;k++)
+				der+=k;
+
+			if(izq==der)
+				return i;
+			if(izq>der)
+				return -1;
+		}
+		return -1;
 	}
 	
 	int lineal(int n) {
-		// (...)
-		return 0;
+		int izq;
+		int der;
+		
+		for(int i=1; i<n; i++) {
+			der = 0;
+			izq = (i*i-i)/2;
+			
+			for(int k=i+1;k<=n;k++)
+				der+=k;
+
+			if(izq==der)
+				return i;
+			if(izq>der)
+				return -1;
+		}
+		return -1;
 	}
 	
 	int constante(int n) {
-		// (...)
-		return 0;
+		if(n<2)
+			return -1;
+		
+		double r = Math.sqrt((n*n+n)/2);
+		return r!=(int)r?-1:(int)r;
 	}
 }
